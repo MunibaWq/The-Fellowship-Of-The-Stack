fc04bad334b8ca138ec1bd3ae674e8d3455dc1a8
danibarker <caseybarker86@gmail.com>
2021-01-05 10:43:58 -0700

    test route for postgres

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const { Pool } = require("pg");
 
-//postgres
+const pool = new Pool({
+    user: 'me',
+    host: 'localhost',
+    database: 'api',
+    password: 'password',
+    port: 5432,
+  })
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
 
 
 let app = express();
@@ -18,3 +18,18 @@ app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM users");
+        const results = result.rows;
+        
+      
+      res.send(results.map(item=>item.name));
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
