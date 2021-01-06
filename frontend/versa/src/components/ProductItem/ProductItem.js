f8bc659bb46fc211a243908538451fd12104ec58
Saunic <saunic8@gmail.com>
2021-01-06 16:20:01 -0700

    made product info json

---
+++ frontend/versa/src/components/ProductItem/ProductItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { productImage, productInfo } from "./ProductItemStyle";
+
+const ProductItem = () => {
+  return (
+    <div>
+      <produtImage />
+      <productInfo>
+        <productName
+      </productInfo>
+    </div>
+  );
+};
+
+export default ProductItem;
---
+++ frontend/versa/src/components/ProductItem/ProductItemStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import {}
+
+const productImage = styled.div`
+    background-image: 
+`;
+
+export def
+
+start an audio call 
+hold on i have to iinstall liveshare audio pparently
+k
+have to end this session to make it work
+
+okay, commit the changes first please
