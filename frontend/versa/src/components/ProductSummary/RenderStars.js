dd421847c708a43ddafaa3b2a59f3611c2ecd538
Muniba Waqar <muniba.wq@gmail.com>
2021-01-07 13:13:24 -0700

    cleaning up warnings

--- frontend/versa/src/components/ProductSummary/RenderStars.js
+++ frontend/versa/src/components/ProductSummary/RenderStars.js
@@ -1,10 +1,9 @@
 import { Star } from "../../images/icons";
 
 const RenderStars = ({ num_stars }) => {
-    console.log(num_stars);
     let starArray = [];
     for (let num = num_stars; num !== 0; num--) {
-        starArray.push(<Star />);
+        starArray.push(<Star key={num} />);
     }
     return starArray;
 };
