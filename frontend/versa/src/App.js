e7b3e3e686fbe20d8ea8f569990958d05bd27343
Saunic <saunic8@gmail.com>
2021-01-07 13:23:41 -0700

    trying to fetch data

--- frontend/versa/src/App.js
+++ frontend/versa/src/App.js
@@ -4,9 +4,16 @@ import { ThemeProvider } from "styled-components";
 import ProductItem from "./pages/ProductItem/ProductItem";
 import Theme from "./toolbox/constants";
 
-// http://localhost:5000/product/{id}
-
 function App() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/product/{id}").then((res) => {
+            setProducts(res.data);
+            console.log(res.data);
+        });
+    });
+
     return (
         <ThemeProvider theme={Theme}>
             <ProductItem />
