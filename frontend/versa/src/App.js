adeab1797830d0903d088a03b5dead86d30d7e01
Saunic <saunic8@gmail.com>
2021-01-07 13:16:34 -0700

    install axios

--- frontend/versa/src/App.js
+++ frontend/versa/src/App.js
@@ -1,6 +1,10 @@
-import ProductItem from "./pages/ProductItem/ProductItem";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { ThemeProvider } from "styled-components";
+import ProductItem from "./pages/ProductItem/ProductItem";
 import Theme from "./toolbox/constants";
+
+// http://localhost:5000/product/{id}
 
 function App() {
     return (
