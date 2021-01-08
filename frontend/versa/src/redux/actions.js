9fea783314efc4b2a45e7a54ccf9ce563680858d
Saunic <saunic8@gmail.com>
2021-01-08 09:48:43 -0700

    tidy up folder architecture

--- frontend/versa/src/redux/actions.js
+++ frontend/versa/src/redux/actions.js
@@ -1,15 +1,5 @@
 import Axios from "axios";
 
-// export const fetchProduct = async (dispatch, getState) => {
-//     const response = await Axios.get("http://localhost:5000/product/1");
-//     console.log(response.data);
-
-//     dispatch({
-//         type: "FETCH_PRODUCT",
-//         payload: response.data,
-//     });
-// };
-
 export const fetchProduct = (id) => {
     return async (dispatch, getState) => {
         const response = await Axios.get("http://localhost:5000/product/" + id);
@@ -35,25 +25,6 @@ export const setPage = (page) => {
         dispatch({
             type: "SET_PAGE",
             payload: page,
-        })
-    }
-}
-
-// const searchUsers = async (text) => {
-//     setLoading();
-//     const res = await github.get(`/search/users?q=${text}`);
-
-//     dispatch({
-//       type: SEARCH_USERS,
-//       payload: res.data.items,
-//     });
-//   };
-
-// export const fetchProduct = async () => {
-//     const response = await Axios.get("http://localhost:5000/product/{id}");
-
-//     return {
-//         type: "FETCH_PRODUCT",
-//         payload: response.data,
-//     };
-// };
+        });
+    };
+};
