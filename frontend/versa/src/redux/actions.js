e6e2dc873be804de8b685883f153c5cff29c7bb6
Muniba Waqar <muniba.wq@gmail.com>
2021-01-07 19:08:41 -0700

    action.js trying some code with axios

--- frontend/versa/src/redux/actions.js
+++ frontend/versa/src/redux/actions.js
@@ -1,8 +1,18 @@
 import Axios from "axios";
 
+// export const fetchProduct = async (dispatch, getState) => {
+//     const response = await Axios.get("http://localhost:5000/product/1");
+//     console.log(response.data);
+
+//     dispatch({
+//         type: "FETCH_PRODUCT",
+//         payload: response.data,
+//     });
+// };
+
 export const fetchProduct = () => {
     return async (dispatch, getState) => {
-        const response = await Axios.get("http://localhost:5000/product/1);
+        const response = await Axios.get("http://localhost:5000/product/1");
         console.log(response.data);
 
         dispatch({
@@ -11,6 +21,16 @@ export const fetchProduct = () => {
         });
     };
 };
+
+// const searchUsers = async (text) => {
+//     setLoading();
+//     const res = await github.get(`/search/users?q=${text}`);
+
+//     dispatch({
+//       type: SEARCH_USERS,
+//       payload: res.data.items,
+//     });
+//   };
 
 // export const fetchProduct = async () => {
 //     const response = await Axios.get("http://localhost:5000/product/{id}");
