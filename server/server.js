78d21501cbf1947588ba3b96db76ddf1e8e89466
MGhebreslassie <messeleghebreslassie@gmail.com>
2021-01-07 20:08:02 -0700

    minor change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ let express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const { Pool } = require("pg");
-
+const cors = require('cors')
 const pool = new Pool({
     user: "me",
     host: "localhost",
@@ -13,6 +13,7 @@ const pool = new Pool({
 
 let app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
