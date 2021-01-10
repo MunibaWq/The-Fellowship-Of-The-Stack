a35615adfb027875601e6ae20530e0a64f7ddbfe
Muniba Waqar <muniba.wq@gmail.com>
2021-01-09 20:41:51 -0700

    routing to product Item page

--- frontend/versa/src/components/Search/ProductCard.js
+++ frontend/versa/src/components/Search/ProductCard.js
@@ -3,25 +3,47 @@ import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { setSelectedProduct, setPage } from "../../redux/actions";
 
+import { useSelector } from "react-redux";
+import ProductItem from "../../pages/ProductItem/ProductItem";
+import { Link, Route, Switch } from "react-router-dom";
+
 const ProductCard = ({ product }) => {
+    const currentProduct = useSelector((state) => state.selectedProduct);
+
     const dispatch = useDispatch();
     const productSelected = (id) => {
         dispatch(setSelectedProduct(id));
         dispatch(setPage("ProductItem"));
+        fetchCurrentProduct();
     };
+    const fetchCurrentProduct = (currentProduct) => {
+        console.log(`go to current product`, currentProduct);
+
+        <Route path="product-item">
+            <ProductItem currentProduct={currentProduct} />
+        </Route>;
+    };
+
     return (
         <ImageCard>
             <Image>
-                <img onClick={() => {
-                    console.log('test')
-                    productSelected(product.id);
-                } }alt = "product"src={`http://localhost:5000/images/${product.image}`} />
+                <img
+                    onClick={() => {
+                        console.log("test");
+                        // productSelected(product.id);
+                        fetchCurrentProduct(currentProduct);
+                    }}
+                    alt="product"
+                    src={`http://localhost:5000/images/${product.image}`}
+                />
             </Image>
             <ProductInfo>
                 <div>
                     <h6>{product.title}</h6>
                 </div>
-                <div><h6>${product.price}</h6></div>
+                <div>
+                    <h6>${product.price}</h6>
+                </div>
             </ProductInfo>
         </ImageCard>
     );
@@ -33,7 +55,6 @@ const ImageCard = styled.div`
     width: 100%;
 `;
 const ProductInfo = styled.div`
-    
     height: 100%;
     background-color: white;
     border-style: solid;
@@ -45,7 +66,7 @@ const ProductInfo = styled.div`
         margin: 0 10px;
     }
     h6 {
-        font-size:smaller;
+        font-size: smaller;
     }
 `;
 const Image = styled.div`
