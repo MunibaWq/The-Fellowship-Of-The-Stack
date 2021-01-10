88f522ff39efcb7960226161465f8cee2012e9dc
Saunic <saunic8@gmail.com>
2021-01-09 17:50:54 -0700

    migrated icons to icons file for easier importing

--- frontend/versa/src/components/Navbar/Navbar.js
+++ frontend/versa/src/components/Navbar/Navbar.js
@@ -1,15 +1,7 @@
 import React from "react";
 import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
 import Logo from "../../images/logo.svg";
-<<<<<<< HEAD
-import WishListIcon from "../../images/Heart.svg";
-import Cart from "../../images/Cart.svg";
-import Shop from "../../images/ShopSearch.svg";
-import Events from "../../images/Events.svg";
-import Home from "../../images/Home.svg";
-=======
 import { ShoppingCart, WishListIcon } from "../../images/icons";
->>>>>>> 7e5d6e2a170ed2438bb741c097e84fce2c5996bb
 
 const Navbar = () => {
     return (
@@ -30,12 +22,8 @@ const Navbar = () => {
                         ACCOUNT
                     </NavLink>
                     <NavLink to="/wishlist" activeStyle>
-<<<<<<< HEAD
-                        <img src={WishListIcon} alt="heart" />
-=======
                         <WishListIcon />
                         {/* <img src={} alt='heart' /> */}
->>>>>>> 7e5d6e2a170ed2438bb741c097e84fce2c5996bb
                     </NavLink>
                     <NavLink to="/shopping-cart" activeStyle>
                         <ShoppingCart />
