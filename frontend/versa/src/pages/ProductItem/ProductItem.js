137a01a793b43e8795bcef527326922cd1ee9e56
Saunic <saunic8@gmail.com>
2021-01-12 15:56:45 -0700

    just to shut probblems up

---
+++ frontend/versa/src/pages/ProductItem/ProductItem.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ArtistDetails from "../../components/Product/ArtistDetails";
+import CustomerReviews from "../../components/Product/CustomerReviewsSection";
+import ProductPageImage from "../../components/Product/ProductPageImage";
+import axios from "axios";
+import ProductData from "../../components/Product/ProductData";
+import { useParams } from "react-router-dom";
+const ProductItem = () => {
+    // const currentProduct = useSelector((state) => state.selectedProduct)
+    const params = useParams();
+    const currentProduct = params.id;
+    const [productDataState, setProductDataState] = useState([]);
+    useEffect(() => {
+        const fetchProduct = async () => {
+            console.log("ProductData.js currentProduct", currentProduct);
+            //add axios to fetch data from API
+            const response = await axios.get(
+                `http://localhost:5000/product/${currentProduct}`
+            );
+
+            console.log(response.data);
+            setProductDataState(response.data);
+        };
+        fetchProduct();
+    }, []);
+    console.log("productItem.js", currentProduct);
+    return (
+        <ProductItemContainer>
+            <ProductPageImage productDataState={productDataState} />
+            <ProductData productDataState={productDataState} />
+            <ArtistDetails />
+            <CustomerReviews />
+        </ProductItemContainer>
+    );
+};
+
+export default ProductItem;
+
+const ProductItemContainer = styled.div`
+    max-width: 425px;
+`;
