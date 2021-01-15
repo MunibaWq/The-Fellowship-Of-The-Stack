42e17cf01601a4ff3a8a1b45edfb2227ba5f93e6
Muniba Waqar <muniba.wq@gmail.com>
2021-01-15 13:27:25 -0700

    addint credentials

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-    user: "me",
-    host: "localhost",
-    database: "api",
-    password: "password",
+    user: `bzwlnqvswwqfqg`,
+    host: `ec2-34-192-72-159.compute-1.amazonaws.com`,
+    database: `d40vu6ijteqrv2`,
+    password: process.env.DBPASSWORD,
     port: 5432,
     max: 50,
 });
-module.exports = pool
+module.exports = pool;
