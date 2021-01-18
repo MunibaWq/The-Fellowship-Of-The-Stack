a06ce49f1be504241e7611d923cc7a12ef8d637d
Saunic <saunic8@gmail.com>
2021-01-18 10:00:56 -0700

    remove customer reviews and artist details

---
+++ frontend/versa/src/components/Reusable/Text.js
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 93px;
+    line-height: 113px;
+    /* identical to box height */
+    letter-spacing: -1.5px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+        letter-spacing: -0.2px;
+    }
+`;
+export const H2 = styled.h2`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 58px;
+    line-height: 70px;
+    letter-spacing: -0.5px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+    }
+`;
+
+export const H3 = styled.h3`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 46px;
+    line-height: 56px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+        letter-spacing: -0.2px;
+    }
+`;
+
+export const H4 = styled.h4`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 33px;
+    line-height: 40px;
+    /* identical to box height */
+    letter-spacing: 0.25px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+        letter-spacing: -0.2px;
+    }
+`;
+export const H5 = styled.h5`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 23px;
+    line-height: 28px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+        letter-spacing: -0.2px;
+    }
+`;
+export const H6 = styled.h6`
+    font-family: "Inter", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 112%;
+    /* or 13px */
+    letter-spacing: 1.6px;
+    text-transform: uppercase;
+    margin-bottom: 8px;
+    @media (max-width: 990px) {
+        font-family: "Inter", sans-serif;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 23.51px;
+        line-height: 106%;
+        /* or 25px */
+        margin-bottom: 16px;
+        letter-spacing: -0.2px;
+    }
+`;
--- frontend/versa/src/pages/UserBuyer/ProductItem/ProductItem.js
+++ frontend/versa/src/pages/UserBuyer/ProductItem/ProductItem.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-
-import ArtistDetails from "../../../components/Product/ArtistDetails"
-import CustomerReviews from "../../../components/Product/CustomerReviewsSection";
 import ProductPageImage from "../../../components/Product/ProductPageImage";
-import { getProductByID } from '../../../axios/gets'
+import { getProductByID } from "../../../axios/gets";
 import ProductData from "../../../components/Product/ProductData";
 import { useParams } from "react-router-dom";
 const ProductItem = () => {
@@ -14,8 +11,7 @@ const ProductItem = () => {
     useEffect(() => {
         const fetchProduct = async () => {
             console.log("ProductData.js currentProduct", currentProduct);
-            const data = await getProductByID(currentProduct)
-            
+            const data = await getProductByID(currentProduct);
 
             console.log(data);
             setProductDataState(data);
@@ -27,8 +23,6 @@ const ProductItem = () => {
         <ProductItemContainer>
             <ProductPageImage productDataState={productDataState} />
             <ProductData productDataState={productDataState} />
-            <ArtistDetails />
-            <CustomerReviews />
         </ProductItemContainer>
     );
 };
