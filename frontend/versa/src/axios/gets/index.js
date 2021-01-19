f4a3ed4e6982882d655352cd112bcaa5e4fb1323
danibarker <caseybarker86@gmail.com>
2021-01-18 17:04:45 -0700

    created function to get Image information from database by product ID

--- frontend/versa/src/axios/gets/index.js
+++ frontend/versa/src/axios/gets/index.js
@@ -1,6 +1,12 @@
 import Axios from 'axios'
 //add axios to fetch data from API
-let host = process.env.NODE_ENV==='production'? "" : "http://localhost:5000"
+let host = process.env.NODE_ENV === 'production' ? "" : "http://localhost:5000"
+export const getImagesByPID = async (currentProduct) => {
+    const response = await Axios.get(
+        `${host}/images/byPID/${currentProduct}`
+    )
+    return response.data
+}
 export const getProductByID = async (currentProduct) => {
     const response = await Axios.get(
         `${host}/products/get/${currentProduct}`
