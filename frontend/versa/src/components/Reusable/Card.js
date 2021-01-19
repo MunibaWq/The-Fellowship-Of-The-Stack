889527a50d6d84bdb57dbef4b95d5e89005b9981
Muniba Waqar <muniba.wq@gmail.com>
2021-01-19 13:52:19 -0700

    creating a card component

---
+++ frontend/versa/src/components/Reusable/Card.js
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+const CardWrapper = styled.div`
+    position: relative;
+    overflow: hidden;
+    width: 300px;
+    padding: 3rem 0 2rem;
+    border-radius: 0.5rem;
+    color: white;
+`;
+
+const Card = () => {
+    return <CardWrapper>Card</CardWrapper>;
+};
