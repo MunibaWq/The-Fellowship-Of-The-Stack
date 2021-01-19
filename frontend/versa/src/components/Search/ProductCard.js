0ca37d0914325da5ff6d2a7aac0616619675ed44
danibarker <caseybarker86@gmail.com>
2021-01-18 17:02:06 -0700

    changed search page to get thumbnail from database if it exists

--- frontend/versa/src/components/Search/ProductCard.js
+++ frontend/versa/src/components/Search/ProductCard.js
@@ -3,43 +3,41 @@ import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { setSelectedProduct } from "../../redux/actions";
 import { Link } from "react-router-dom";
-let host = process.env.NODE_ENV==='production'? "" : "http://localhost:5000"
+let host = process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
 const ProductCard = ({ product }) => {
-    
-    console.log('loaded')
     const dispatch = useDispatch();
     const productSelected = (id) => {
-        console.log('here')
         dispatch(setSelectedProduct(id));
-        
     };
-
-    
 
     return (
         <Link to={`/product-item/${product.id}`}>
-        <ImageCard>
-            <Image>
-                <img
-                    onClick={() => {
-                        console.log("test");
-                        productSelected(product.id);
-                        // fetchCurrentProduct(currentProduct);
-                    }}
-                    alt="product"
-                    src={`${host}/images/${product.image}`}
-                />
-            </Image>
-            <ProductInfo>
-                <div>
-                    <h6>{product.title}</h6>
-                </div>
-                <div>
-                    <h6>${product.price}</h6>
-                </div>
-            </ProductInfo>
-        </ImageCard>
-    </Link>
+            <ImageCard>
+                <Image>
+                    <img
+                        onClick={() => {
+                            productSelected(product.id);
+                        }}
+                        alt="product"
+                        src={
+                            product.image
+                                ? `${host}/images/${product.image}`
+                                : product.thumbnail
+                                ? `https://versabucket.s3.us-east-2.amazonaws.com/images/${product.thumbnail}`
+                                : ""
+                        }
+                    />
+                </Image>
+                <ProductInfo>
+                    <div>
+                        <h6>{product.title}</h6>
+                    </div>
+                    <div>
+                        <h6>${product.price}</h6>
+                    </div>
+                </ProductInfo>
+            </ImageCard>
+        </Link>
     );
 };
 const ImageCard = styled.div`
@@ -47,7 +45,7 @@ const ImageCard = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height:95%;
+    height: 95%;
 `;
 const ProductInfo = styled.div`
     height: 100%;
