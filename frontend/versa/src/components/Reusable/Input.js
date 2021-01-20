32f6086423514bde7e42faf27d4e74833408013c
danibarker <caseybarker86@gmail.com>
2021-01-20 12:00:56 -0700

    replacing hardcoded color in Input.js with color from Colors.js

--- frontend/versa/src/components/Reusable/Input.js
+++ frontend/versa/src/components/Reusable/Input.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
+import colors from "./Colors";
+
 export const TextField = ({ multi, label, tests = [] }) => {
     const [showError, setShowError] = useState([]);
     return (
@@ -47,15 +49,15 @@ export const TextArea = styled.textarea`
     border-radius: 5px;
     border-style: none;
     height: 100px;
-    padding-left:10px;
+    padding-left: 10px;
     background-color: rgba(80, 80, 80, 15%);
     @media screen and (max-width: 450px) {
         width: 100%;
     }
     &:focus {
         outline: none !important;
-        border: 3px solid #6495ed;
-        box-shadow: 0 0 10px #6495ed;
+        border: 3px solid ${colors.primary};
+        box-shadow: 0 0 10px ${colors.primary};
     }
 `;
 export const Label = styled.label``;
@@ -66,16 +68,15 @@ export const Error = styled.p`
 export const Input = styled.input`
     border-radius: 5px;
     border-style: none;
-    padding-left:10px;
+    padding-left: 10px;
     height: 35px;
-    margin:10px;
+    margin: 10px;
     background-color: rgba(80, 80, 80, 15%);
 
-    &:focus{
-            outline: none !important;
-            border:3px solid #6495ed;
-            box-shadow: 0 0 10px #6495ed;
-        }
+    &:focus {
+        outline: none !important;
+        border: 3px solid ${colors.primary};
+        box-shadow: 0 0 10px ${colors.primary};
     }
 `;
 
@@ -85,7 +86,7 @@ export const ColorInput = styled.input.attrs((props) => ({
     border-radius: 100%;
     height: 35px;
     width: auto;
-    margin:10px;
+    margin: 10px;
     border: none;
     outline: none;
     -webkit-appearance: none;
