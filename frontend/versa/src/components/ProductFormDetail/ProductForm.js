e9a35b83875e6e779b4e8d09ebdc42e6a260f891
Saunic <saunic8@gmail.com>
2021-01-20 15:29:26 -0700

    trying to make variation list

--- frontend/versa/src/components/ProductFormDetail/ProductForm.js
+++ frontend/versa/src/components/ProductFormDetail/ProductForm.js
@@ -1,10 +1,108 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { TextField } from "../Reusable/Input";
 import Button from "../Reusable/Button";
 import Icon from "../Reusable/Icons";
-
+import VariationsList from "./VariationsList";
 const ProductForm = (props) => {
+    const setDefault = (fieldName) => {
+        if (localStorage.getItem(`${fieldName}`)) {
+            return localStorage.getItem(`${fieldName}`);
+        } else {
+            return "";
+        }
+    };
+    const setDefault2 = (arrName) => {
+        if (localStorage.getItem(`${arrName}`)) {
+            return JSON.parse(localStorage.getItem(`${arrName}`));
+        } else {
+            return [];
+        }
+    };
+
+    const { register, errors } = useForm();
+    const [colors, setColors] = useState(setDefault2("productColors"));
+    const [sizes, setSizes] = useState(setDefault2("productSizes"));
+    const [images, setImages] = useState([]);
+    const [inputName, setInputName] = useState(setDefault("productName"));
+    const [inputPrice, setInputPrice] = useState(setDefault("productPrice"));
+    const [inputDesc, setInputDesc] = useState(setDefault("productDesc"));
+    const [inputFit, setInputFit] = useState(setDefault("productFit"));
+    const [inputMaterials, setInputMaterials] = useState(
+        setDefault("productMaterials")
+    );
+    const [checkDelete, setCheckDelete] = useState(false);
+
+    function clearField() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+        setColors([]);
+        setSizes([]);
+        setImages([]);
+        setInputName("");
+        setInputPrice("");
+        setInputDesc("");
+        setInputFit("");
+        setInputMaterials("");
+        colorPick.current.value = "";
+        colorValue.current.value = "#000000";
+        sizePrice.current.value = "";
+    }
+
+    useEffect(() => {
+        localStorage.setItem("productName", inputName);
+        localStorage.setItem("productPrice", inputPrice);
+        localStorage.setItem("productDesc", inputDesc);
+        localStorage.setItem("productFit", inputFit);
+        localStorage.setItem("productMaterials", inputMaterials);
+        localStorage.setItem("productColors", JSON.stringify(colors));
+        localStorage.setItem("productSizes", JSON.stringify(sizes));
+    }, [
+        inputName,
+        inputPrice,
+        inputDesc,
+        inputFit,
+        inputMaterials,
+        colors,
+        sizes,
+    ]);
+
+    const colorValue = useRef();
+    const colorPick = useRef();
+    const sizeLabel = useRef();
+    const sizePrice = useRef();
+    const addColor = useRef();
+
+    function setColorLabelAndValue(e) {
+        if (!colorPick.current.value) {
+            return;
+        }
+        if (colors.length < 3) {
+            let temp = {
+                label: colorPick.current.value,
+                value: colorValue.current.value,
+            };
+            let appendedColors = colors.concat([temp]);
+            setColors(appendedColors);
+        }
+    }
+    function setSizeLabelAndPrice() {
+        let temp = {
+            label: sizeLabel.current.value,
+            price: sizePrice.current.value,
+        };
+        let appendedSizes = sizes.concat([temp]);
+        setSizes(appendedSizes);
+    }
+    function deleteItem(index, arr) {
+        arr.splice(index, 1);
+        localStorage.setItem(`product${arr}`, JSON.stringify(arr));
+        setCheckDelete(!checkDelete);
+    }
     return (
         <Form>
             <h2>Product Details</h2>
@@ -21,6 +119,11 @@ const ProductForm = (props) => {
                     },
                 ]}
                 label="Product Name"
+                ref={register({ required: true, minLength: 2 })}
+                value={inputName}
+                onChange={(e) => {
+                    setInputName(e.target.value);
+                }}
             ></TextField>
 
             <TextField
@@ -40,6 +143,17 @@ const ProductForm = (props) => {
                     },
                 ]}
                 label="Price"
+                ref={register({
+                    required: true,
+                    valueAsNumber: true,
+                })}
+                value={inputPrice}
+                onWheel={(e) => {
+                    e.preventDefault();
+                }}
+                onChange={(e) => {
+                    setInputPrice(e.target.value);
+                }}
             ></TextField>
             <TextField
                 multi={true}
@@ -50,6 +164,11 @@ const ProductForm = (props) => {
                     },
                 ]}
                 label="Description"
+                ref={register({ required: true, minLength: 2 })}
+                value={inputDesc}
+                onChange={(e) => {
+                    setInputDesc(e.target.value);
+                }}
             ></TextField>
             <TextField
                 multi={true}
@@ -60,12 +179,18 @@ const ProductForm = (props) => {
                     },
                 ]}
                 label="Materials"
+                ref={register({ required: true, minLength: 2 })}
+                value={inputMaterials}
+                onChange={(e) => {
+                    setInputMaterials(e.target.value);
+                }}
             ></TextField>
             <h2>Colours</h2>
             <Button secondary>
                 Add
                 <Icon type="add" />
             </Button>
+            <VariationsList variation="colour" colors images sizes />
             <h2>Sizes</h2>
             <Button secondary>
                 Add <Icon type="add" />
@@ -75,7 +200,7 @@ const ProductForm = (props) => {
                 Add <Icon type="add" />
             </Button>
             <Container>
-                <Button>
+                <Button onClick={clearField}>
                     Cancel
                     <Icon type="lineClose" />
                 </Button>
@@ -83,17 +208,46 @@ const ProductForm = (props) => {
                     Submit
                 </Button>
             </Container>
+            <Error>
+                {errors.product_name?.type === "required" &&
+                    "Input is required."}
+            </Error>
+            <Error>
+                {errors.product_name?.type === "minLength" &&
+                    "Must be at least 2 characters."}
+            </Error>
+            <Error>
+                {errors.product_price?.type === "required" &&
+                    "Input is required."}
+            </Error>
+            <Error>
+                {errors.product_description?.type === "required" &&
+                    "Input is required."}
+            </Error>
+            <Error>
+                {errors.product_description?.type === "minLength" &&
+                    "Must be at least 2 characters."}
+            </Error>
+            <Error>
+                {errors.product_materials?.type === "required" &&
+                    "Input is required."}
+            </Error>
+
+            <Error>
+                {errors.product_materials?.type === "minLength" &&
+                    "Must be at least 2 characters."}
+            </Error>
         </Form>
     );
 };
 
 export default ProductForm;
 
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`;
+const Form = styled.form``;
 const Container = styled.div`
     display: flex;
+`;
+
+const Error = styled.p`
+    color: red;
 `;
--- frontend/versa/src/components/ProductFormDetail/VariationsList.js
+++ frontend/versa/src/components/ProductFormDetail/VariationsList.js
@@ -1,36 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Icons from "../Reusable/Icons";
 
-const VariationsList = (props, colors) => {
-    switch (props.type) {
+const VariationsList = (props, variation, colors, sizes, images) => {
+    const [checkDelete, setCheckDelete] = useState(false);
+
+    function deleteItem(index, arr) {
+        arr.splice(index, 1);
+        localStorage.setItem(`product${arr}`, JSON.stringify(arr));
+        setCheckDelete(!checkDelete);
+    }
+
+    variation.length > 0 &&
+        
+    switch (variation) {
+        case "colors":
+
+    }
+            {colors.length > 0 &&
+                colors.map((label, index) => {
+                    return (
+                        <CurrentColour>
+                            <ColourPreview
+                                colour={colors.label.value}
+                            />
+                            <p>{colors.label.label}</p>
+                            <div
+                                onClick={() =>
+                                    deleteItem(index, colors)
+                                }
+                            >
+                                <Icons lineClose />
+                            </div>
+                        </CurrentColour>
+                        );
+                })}
+            
+        }
+    }
+
+    
+
+    switch (props.variation) {
         case "colour":
+            {
+                        
+
+        case "size":
             return (
-                <NewColourContainer>
-                    {colors.length > 0 &&
-                        colors.map((label, index) => {
+                <Container>
+                    {sizes.length > 0 &&
+                        sizes.map((size, index) => {
                             return (
-                                <CurrentColour>
-                                    <ColourPreview colour={label.value} />
-                                    <p>{label.label}</p>
+                                <NewSize>
+                                    <p>{size.label}</p>
+
+                                    <NewSizePrice>$ {size.price}</NewSizePrice>
                                     <div
-                                        onClick={() =>
-                                            deleteItem(index, colors)
-                                        }
+                                        onClick={() => deleteItem(index, sizes)}
                                     >
                                         <Icons lineClose />
                                     </div>
-                                </CurrentColour>
+                                </NewSize>
                             );
                         })}
-                </NewColourContainer>
+                </Container>
             );
+        case "images":
+            return (
+                <Container>
+                    {images.map((image, index) => {
+                        return (
+                            <>
+                                <UploadedImage
+                                    key={index}
+                                    alt="product"
+                                    src={image.image}
+                                />
+                            </>
+                        );
+                    })}
+                </Container>
+            );
+        default:
+            return <></>;
     }
 };
 
 export default VariationsList;
 
-const NewColourContainer = styled.div`
+const Container = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: flex-start;
@@ -59,4 +118,30 @@ const ColourPreview = styled.div`
     margin-right: 20px;
     border-radius: 50%;
     background-color: ${(props) => props.colour};
+`;
+
+const NewSize = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 10px;
+    background: #c5c3ff;
+    margin: 8px;
+
+    p {
+        margin-right: 10px;
+        text-transform: uppercase;
+        font-weight: 700;
+        letter-spacing: 0.05em;
+    }
+`;
+
+const NewSizePrice = styled.p`
+    margin-right: 10px;
+`;
+
+const UploadedImage = styled.img`
+    width: 200px;
+    height: 200px;
 `;
