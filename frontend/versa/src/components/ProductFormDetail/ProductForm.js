d4744bf5b661821d60c3bb10f798b447c811991c
danibarker <caseybarker86@gmail.com>
2021-01-20 15:18:05 -0700

    too long of message

--- frontend/versa/src/components/ProductFormDetail/ProductForm.js
+++ frontend/versa/src/components/ProductFormDetail/ProductForm.js
@@ -46,7 +46,7 @@ const ProductForm = (props) => {
                 tests={[
                     {
                         test: (input) => input.length < 10,
-                        error: "Product description is required.  Minimum 10 characters",
+                        error: "Product description is required.",
                     },
                 ]}
                 label="Description"
