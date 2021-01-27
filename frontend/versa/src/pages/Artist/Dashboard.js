411b42580b51a599e7d6d99ee6faface947114bb
Muniba Waqar <muniba.wq@gmail.com>
2021-01-27 14:16:38 -0700

    Called all products in dashboard based on id

--- frontend/versa/src/pages/Artist/Dashboard.js
+++ frontend/versa/src/pages/Artist/Dashboard.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { getAllProducts } from "../../axios/gets";
 
 const Dashboard = () => {
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        const getProducts = async () => {
+            let data = await getAllProducts();
+            console.log(data);
+        };
+        getProducts();
+    }, []);
+
     return (
         <div style={{ padding: "2%" }}>
             <table style={{ width: "100%" }}>
