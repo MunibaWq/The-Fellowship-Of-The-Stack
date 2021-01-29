df848a76713d09e81d82377a28fb5660402075c8
Saunic <saunic8@gmail.com>
2021-01-29 07:36:34 -0700

    fix differing stroke widths for svg on nav

--- frontend/versa/src/images/icons.js
+++ frontend/versa/src/images/icons.js
@@ -187,7 +187,7 @@ export const CategoriesIcon = ({ width, height, stroke, fill }) => {
         </svg>
     );
 };
-export const Magnifying = ({ width, height, stroke, fill }) => {
+export const Magnifying = ({ width, height, stroke, fill, strokeWidth }) => {
     return (
         <svg
             width={width || "26"}
@@ -199,14 +199,14 @@ export const Magnifying = ({ width, height, stroke, fill }) => {
             <path
                 d="M14.4998 25C20.2987 25 24.9998 20.299 24.9998 14.5C24.9998 8.70101 20.2987 4 14.4998 4C8.70077 4 3.99976 8.70101 3.99976 14.5C3.99976 20.299 8.70077 25 14.4998 25Z"
                 stroke={stroke || "black"}
-                strokeWidth="1.5"
+                strokeWidth={strokeWidth || "2"}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M21.924 21.925L27.999 28.0001"
                 stroke={stroke || "black"}
-                strokeWidth="1.5"
+                strokeWidth={strokeWidth || "2"}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
@@ -290,28 +290,28 @@ export const ShoppingCart = ({ width, height, stroke, fill }) => {
             <path
                 d="M24 23.521H8.56185C8.32766 23.521 8.1009 23.4388 7.9211 23.2887C7.7413 23.1387 7.61987 22.9303 7.57797 22.6999L4.2402 4.34211C4.19831 4.1117 4.07688 3.9033 3.89708 3.75324C3.71728 3.60319 3.49052 3.521 3.25633 3.521H1"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M9 28.521C10.3807 28.521 11.5 27.4017 11.5 26.021C11.5 24.6403 10.3807 23.521 9 23.521C7.61929 23.521 6.5 24.6403 6.5 26.021C6.5 27.4017 7.61929 28.521 9 28.521Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M24 28.521C25.3807 28.521 26.5 27.4017 26.5 26.021C26.5 24.6403 25.3807 23.521 24 23.521C22.6193 23.521 21.5 24.6403 21.5 26.021C21.5 27.4017 22.6193 28.521 24 28.521Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M5 8.521H26.8018C26.9483 8.521 27.0929 8.55317 27.2256 8.61525C27.3583 8.67733 27.4757 8.76779 27.5695 8.88025C27.6634 8.9927 27.7314 9.1244 27.7687 9.26603C27.8061 9.40766 27.8119 9.55577 27.7857 9.69988L26.1493 18.6999C26.1074 18.9303 25.986 19.1387 25.8062 19.2887C25.6264 19.4388 25.3996 19.521 25.1654 19.521H7"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
@@ -351,35 +351,35 @@ export const EventsIcon = ({ width, height, stroke, fill }) => {
             <path
                 d="M26 5H6C5.44772 5 5 5.44772 5 6V26C5 26.5523 5.44772 27 6 27H26C26.5523 27 27 26.5523 27 26V6C27 5.44772 26.5523 5 26 5Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M22 3V7"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M10 3V7"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M5 11H27"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M15.6437 14.0181C15.7922 13.7272 16.2078 13.7272 16.3563 14.0181L17.3327 15.9314L17.8671 15.6587L17.3327 15.9314C17.5652 16.387 18.0015 16.7039 18.5066 16.7843L20.628 17.1217C20.9505 17.173 21.079 17.5683 20.8482 17.7994L19.3302 19.3192C18.9688 19.6811 18.8022 20.194 18.8819 20.6992L19.2165 22.821C19.2674 23.1436 18.9311 23.3879 18.64 23.2399L16.7255 22.2659C16.2696 22.034 15.7304 22.034 15.2745 22.2659L13.36 23.2399C13.0689 23.3879 12.7326 23.1436 12.7835 22.821L13.1181 20.6992C13.1978 20.194 13.0312 19.6811 12.6698 19.3193L11.1518 17.7994C10.921 17.5683 11.0495 17.173 11.372 17.1217L13.4934 16.7843C13.9985 16.7039 14.4348 16.387 14.6673 15.9314L15.6437 14.0181Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.2"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
@@ -399,14 +399,14 @@ export const AccountIcon = ({ width, height, stroke, fill }) => {
             <path
                 d="M16 20C20.4183 20 24 16.4183 24 12C24 7.58172 20.4183 4 16 4C11.5817 4 8 7.58172 8 12C8 16.4183 11.5817 20 16 20Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M3.87363 26.9988C5.10308 24.8708 6.87089 23.1037 8.99948 21.8752C11.1281 20.6467 13.5425 20 16.0001 20C18.4578 20 20.8722 20.6468 23.0008 21.8754C25.1293 23.1039 26.8971 24.871 28.1265 26.9991"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
@@ -426,28 +426,28 @@ export const CartIcon = ({ width, height, stroke, fill }) => {
             <path
                 d="M24 23H8.56185C8.32766 23 8.1009 22.9178 7.9211 22.7678C7.7413 22.6177 7.61987 22.4093 7.57797 22.1789L4.2402 3.82112C4.19831 3.5907 4.07688 3.3823 3.89708 3.23225C3.71728 3.08219 3.49052 3 3.25633 3H1"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M9 28C10.3807 28 11.5 26.8807 11.5 25.5C11.5 24.1193 10.3807 23 9 23C7.61929 23 6.5 24.1193 6.5 25.5C6.5 26.8807 7.61929 28 9 28Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M24 28C25.3807 28 26.5 26.8807 26.5 25.5C26.5 24.1193 25.3807 23 24 23C22.6193 23 21.5 24.1193 21.5 25.5C21.5 26.8807 22.6193 28 24 28Z"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M5 8H26.8018C26.9483 8 27.0929 8.03218 27.2256 8.09425C27.3583 8.15633 27.4757 8.2468 27.5695 8.35925C27.6634 8.4717 27.7314 8.6034 27.7687 8.74504C27.8061 8.88667 27.8119 9.03478 27.7857 9.17889L26.1493 18.1789C26.1074 18.4093 25.986 18.6177 25.8062 18.7678C25.6264 18.9178 25.3996 19 25.1654 19H7"
                 stroke={stroke || "#444444"}
-                strokeWidth="1.5"
+                strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
