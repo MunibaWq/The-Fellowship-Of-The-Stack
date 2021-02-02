c865c1c00457f8b45da367d24db7c1c1094e7154
Saunic <saunic8@gmail.com>
2021-02-02 15:00:18 -0700

    finish events results page

--- frontend/versa/src/pages/Events.js
+++ frontend/versa/src/pages/Events.js
@@ -1,28 +1,77 @@
 import React from "react";
 import styled from "styled-components";
 import EventCard from "../components/Events/EventCard";
+import { Magnifying } from "../images/icons";
+import Loading from "../components/Reusable/Loading";
+import theme from "../components/Reusable/Colors.js";
 
 const Events = () => {
+    // const search = async () => {
+    //     let data = await searchEvents(query);
+    //     setEvents(data);
+    // };
     return (
         <EventsResults>
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
+            {/**<SearchBarDiv>
+                <MagnifyIcon
+                    onClick={() => {
+                        if (query) {
+                            search();
+                        }
+                    }}
+                >
+                    <Magnifying stroke={theme.primary} strokeWidth="4" />
+                </MagnifyIcon>
+                <SearchBar
+                    onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                            search();
+                        }
+                    }}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Search"
+                    type="text"
+                />
+            </SearchBarDiv>**/}
+            <Results>
+                {/**{!events ? (
+                    <Loading />
+                ) : events.length > 0 ? (
+                    events.map((product, index) => (
+                        <EventCard key={index} event={event} />
+                    ))
+                ) : (
+                    <NoResultsMessage>No results found</NoResultsMessage>
+                )}**/}
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+            </Results>
         </EventsResults>
     );
 };
 
 export default Events;
 
-const EventsResults = styled.div``;
+const EventsResults = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Results = styled.div`
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    margin: 1.5em;
+`;
