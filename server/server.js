7135cf17ce5441f1fdf72ce634627f25fb8743dc
MGhebreslassie <messeleghebreslassie@gmail.com>
2021-02-04 17:36:23 -0700

    moved stock routes to own router.

--- server/server.js
+++ server/server.js
@@ -4,14 +4,15 @@ const crypto = require("crypto");
 const path = require("path");
 const userRouter = require("./routers/userRouter");
 const PORT = process.env.PORT || 5000;
+const stockRouter = require("./routers/stockRouter");
 const imageRouter = require("./routers/imageRouter");
 const productRouter = require("./routers/productRouter");
-const eventRouter = require('./routers/eventRouter')
-var cookieParser = require('cookie-parser');
+const eventRouter = require("./routers/eventRouter");
+var cookieParser = require("cookie-parser");
 let app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({credentials: true }));
+app.use(cors({ credentials: true }));
 
 app.use(express.static(path.join(__dirname, "../frontend/versa/build")));
 
@@ -22,11 +23,11 @@ app.listen(PORT, () => {
 app.use(express.static("../frontend/versa/build"));
 
 //ROUTES
-
+app.use("/stock", stockRouter);
 app.use("/images", imageRouter);
 app.use("/products", productRouter);
 app.use("/users", userRouter);
-app.use('/events', eventRouter)
+app.use("/events", eventRouter);
 app.get("*", (req, res) => {
     res.sendFile(
         path.resolve(__dirname, "../frontend/versa/build", "index.html")
