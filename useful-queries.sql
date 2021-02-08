118fdd5ab39b3592b15221eac305d85c17f80cc4
danibarker <caseybarker86@gmail.com>
2021-02-07 22:58:58 -0700

    more queries

--- useful-queries.sql
+++ useful-queries.sql
@@ -14,5 +14,19 @@ SELECT sum(sale_price), EXTRACT(day FROM DATE) AS DAY, EXTRACT(month FROM DATE) 
 FROM sales_by_product
 GROUP BY EXTRACT(day FROM DATE), EXTRACT(month FROM DATE), EXTRACT(year FROM DATE)
 ORDER BY DAY desc;
-/*This will get 
+/*This will get sales by day*/
+
+SELECT p.title, s.sale_price, SUM(s.sale_price * s.quantity) 
+FROM sales_by_product s
+INNER JOIN products p
+ON p.id=s.product_id
+GROUP BY product_id, title, s.sale_price
+/* this will get sales by product*/
+
+SELECT p.title, COUNT(s.sale_price) AS quantity, SUM(s.sale_price * s.quantity) 
+FROM sales_by_product s
+INNER JOIN products p
+ON p.id=s.product_id
+GROUP BY product_id, title
+/* total orders page*/
 
