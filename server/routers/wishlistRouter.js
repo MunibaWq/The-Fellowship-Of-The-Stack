c9d613a0182ca1f898b88358508c5d84d8ed2f4d
Muniba Waqar <muniba.wq@gmail.com>
2021-02-15 14:17:47 -0700

    added wishlist put endpoint

--- server/routers/wishlistRouter.js
+++ server/routers/wishlistRouter.js
@@ -5,16 +5,42 @@ const router = new express.Router();
 const auth = require("../middleware/auth");
 
 router.get("/", auth, async (req, res) => {
-    const userID = req.user.id || req.params.session;
-    const wishlistQuery = await pool.query(
-        "SELECT * FROM wishlist WHERE user_id = " + userID
-    );
-    const wishlistID = wishlistQuery.rows[0].id;
-    const itemsQuery = await pool.query(
-        `SELECT w.*, p.thumbnail, p.sizes, p.title, p.price FROM wishlist_items W 
-        INNER JOIN products p ON p.id = c.product_id 
+    if (!req.user.is_artist || !req.user.is_buyer) {
+        res.status(500).send("Not Authorized");
+    } else {
+        const userID = req.user.id;
+        const wishlistQuery = await pool.query(
+            "SELECT * FROM wishlist WHERE user_id = " + userID
+        );
+        const wishlistID = wishlistQuery.rows[0].id;
+        const itemsQuery = await pool.query(
+            `SELECT w.*, p.thumbnail, p.sizes, p.title, p.price FROM wishlist_items W 
+        INNER JOIN products p ON p.id = w.product_id 
         WHERE w.wishlist_id = ${wishlistID}`
-    );
-    const items = itemsQuery.rows;
-    res.status(200).json(items);
+        );
+        const items = itemsQuery.rows;
+        res.status(200).json(items);
+    }
+});
+
+router.put("/add", auth, async (req, res) => {
+    if (!req.user.is_artist || !req.user.is_buyer) {
+        res.status(500).send("Not Authorized");
+    } else {
+        try {
+            let { id, productID, wishlistID } = req.body.data;
+
+            let wishlistInfo = await pool.query(
+                `INSERT INTO wishlist_items (product_id,wishlist_id)
+                VALUES ($1,$2) RETURNING id `,
+                [productID, wishlistID]
+            );
+            res.json(wishlistInfo.rows[0].id);
+        } catch (err) {
+            console.error(err.message);
+            res.send({
+                message: "error",
+            });
+        }
+    }
 });
