47ae393c6f6305986f14949b511ed095e82d5ee8
danibarker <caseybarker86@gmail.com>
2021-02-15 11:26:34 -0700

    delivery queries added to useful-queries

--- useful-queries.sql
+++ useful-queries.sql
@@ -30,3 +30,16 @@ ON p.id=s.product_id
 GROUP BY product_id, title
 /* total orders page*/
 
+
+SELECT * FROM orders WHERE deliverer_id IS NULL AND status = 'Ready for Delivery';
+/* select orders that are ready for delivery and aren't chosen by a deliverer yet */
+SELECT * FROM orders WHERE deliverer_id = 20 AND status = 'Delivered';
+/* select past orders that user 20 has delivered */
+SELECT * FROM orders WHERE deliverer_id = 20 AND status = 'Delivering';
+
+SELECT a.store_address, p.title, p.artist_id, o.* FROM order_items o 
+INNER JOIN products p ON p.id=o.product_id 
+INNER JOIN users a ON a.id = p.artist_id WHERE order_id = 53;
+
+/* select orders that are chosen by user 20 to be currently delivering */
+/* select order_items for order 53*/
