e1390a87e021e57a7b940aead6313ceb960fa57b
Muniba Waqar <muniba.wq@gmail.com>
2021-02-15 14:27:01 -0700

    delete enpoint for wishlist items

--- server/routers/wishlistRouter.js
+++ server/routers/wishlistRouter.js
@@ -8,18 +8,34 @@ router.get("/", auth, async (req, res) => {
     if (!req.user.is_artist || !req.user.is_buyer) {
         res.status(500).send("Not Authorized");
     } else {
-        const userID = req.user.id;
-        const wishlistQuery = await pool.query(
-            "SELECT * FROM wishlist WHERE user_id = " + userID
+        let checkOwner = await pool.query(
+            "SELECT user_id from wishlist WHERE id = " + id
         );
-        const wishlistID = wishlistQuery.rows[0].id;
-        const itemsQuery = await pool.query(
-            `SELECT w.*, p.thumbnail, p.sizes, p.title, p.price FROM wishlist_items W 
+        if (checkOwner.rows[0].user_id !== req.user.id) {
+            res.status(500).send("Not Authorized");
+        }
+        if (Object.keys(req.params).length === 0) {
+            console.log("no id");
+        }
+        try {
+            const userID = req.user.id;
+            const wishlistQuery = await pool.query(
+                "SELECT * FROM wishlist WHERE user_id = " + userID
+            );
+            const wishlistID = wishlistQuery.rows[0].id;
+            const itemsQuery = await pool.query(
+                `SELECT w.*, p.thumbnail, p.sizes, p.title, p.price FROM wishlist_items W 
         INNER JOIN products p ON p.id = w.product_id 
         WHERE w.wishlist_id = ${wishlistID}`
-        );
-        const items = itemsQuery.rows;
-        res.status(200).json(items);
+            );
+            const items = itemsQuery.rows;
+            res.status(200).json(items);
+        } catch (err) {
+            console.error(err.message);
+            res.send({
+                message: "error",
+            });
+        }
     }
 });
 
@@ -27,6 +43,15 @@ router.put("/add", auth, async (req, res) => {
     if (!req.user.is_artist || !req.user.is_buyer) {
         res.status(500).send("Not Authorized");
     } else {
+        let checkOwner = await pool.query(
+            "SELECT user_id from wishlist WHERE id = " + id
+        );
+        if (checkOwner.rows[0].user_id !== req.user.id) {
+            res.status(500).send("Not Authorized");
+        }
+        if (Object.keys(req.params).length === 0) {
+            console.log("no id");
+        }
         try {
             let { id, productID, wishlistID } = req.body.data;
 
@@ -44,3 +69,35 @@ router.put("/add", auth, async (req, res) => {
         }
     }
 });
+
+router.delete("/delete/:id", auth, async (req, res) => {
+    const id = req.params.id;
+    if (!req.user.is_artist || !req.user.is_buyer) {
+        res.status(500).send("Not Authorized");
+    } else {
+        let checkOwner = await pool.query(
+            "SELECT user_id from wishlist WHERE id = " + id
+        );
+        if (checkOwner.rows[0].user_id !== req.user.id) {
+            res.status(500).send("Not Authorized");
+        }
+        if (Object.keys(req.params).length === 0) {
+            console.log("no id");
+        }
+        try {
+            await pool.query(
+                "DELETE FROM wishlist_items WHERE product_id = $1",
+                [id]
+            );
+
+            res.json({ msg: "Wishlist Item Deleted!" });
+        } catch (err) {
+            console.error(err.message);
+            res.send({
+                message: "error",
+            });
+        }
+    }
+});
+
+module.exports = router;
