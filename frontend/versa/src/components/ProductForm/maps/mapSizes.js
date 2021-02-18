e6c0c3c16dbc4191b64c8ca5a2204b5e9f6cf8ae
MGhebreslassie <messeleghebreslassie@gmail.com>
2021-02-18 00:10:25 -0700

    added manufacture cost to size label

--- frontend/versa/src/components/ProductForm/maps/mapSizes.js
+++ frontend/versa/src/components/ProductForm/maps/mapSizes.js
@@ -1,24 +1,17 @@
 import React from "react";
 import { LineCloseIcon } from "../../../images/icons";
-import {
-    RemoveIcon,
-
-    NewSize,
-    NewSizePrice
-} from "../styledComponents";
+import { RemoveIcon, NewSize, NewSizePrice } from "../styledComponents";
 import { deleteItem } from "../functions/deleteItem";
 import { setFormInputs } from "../../../redux/actions/Forms";
 
 export function mapSizes(sizes, dispatch) {
-
     return sizes.map((size, index) => {
         return (
             <NewSize>
                 <p>{size.label}</p>
 
-                <NewSizePrice>
-                    $ {size.price}
-                </NewSizePrice>
+                <NewSizePrice>$ {size.price}</NewSizePrice>
+                <NewSizePrice>$ {size.cost || "??"}</NewSizePrice>
                 <RemoveIcon
                     onClick={() => {
                         deleteItem(
@@ -26,10 +19,9 @@ export function mapSizes(sizes, dispatch) {
                             sizes,
                             setFormInputs,
                             dispatch,
-                            'sizes'
+                            "sizes"
                         );
-                    }}
-                >
+                    }}>
                     <LineCloseIcon stroke="white" />
                 </RemoveIcon>
             </NewSize>
