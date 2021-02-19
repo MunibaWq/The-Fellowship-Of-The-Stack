f3d56bb59a16b4b93e980944e4e18c772db1bcb8
Muniba Waqar <muniba.wq@gmail.com>
2021-02-19 14:28:36 -0700

    working on messages get router

--- server/routers/messageRouter.js
+++ server/routers/messageRouter.js
@@ -1,10 +1,30 @@
-
 const router = require("express").Router();
 const pool = require("../db");
 const auth = require("../middleware/auth");
 
-router.get()
 
-router.post()
+    router.get("/get", async (req, res => {
+        try {
+            const result = await pool.query(`SELECT 
+            INNER JOIN
+            messages m
+            ON u.id= m.id
+          
+            INNER JOIN users u
+            ON u.id = m.from
+            INNER JOIN users u
+            ON u.id = m.to
+            `);
+            const results = result.rows;
+    
+            res.json(results);
+        } catch (e) {
+            console.log(e, "/get");
+            res.send("error");
+        }
+    }));
+
+
+router.post();
 
 module.exports = router;
