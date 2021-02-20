7172a3ec801ecb7dd4f047f894b0eb6dbdc22ecf
danibarker <caseybarker86@gmail.com>
2021-02-20 06:43:41 -0700

    create axios calls to send message and to send read receipts

--- frontend/versa/src/axios/posts/index.js
+++ frontend/versa/src/axios/posts/index.js
@@ -157,59 +157,10 @@ export const addToNewsletterList = async (email) => {
     }
 };
 
-/**
- * {
- *   Response: {
- *     bucket: "myBucket",
- *     key: "image/test-image.jpg",
- *     location: "https://myBucket.s3.amazonaws.com/media/test-file.jpg"
- *   }
- * }
- */
+export const readMessage = (topic, to) => {
+    Axios.post("/api/messages/read", { topic, to });
+};
 
-// var s3 = new AWS.S3({
-//     aws_access_key_id: accessKeyId,
-//     aws_secret_access_key: secretKey,
-// });
-
-// // configuring parameters
-// var params = {
-//     Bucket: BUCKET_NAME,
-//     Body: image,
-//     Key: "images/" + filename,
-// };
-
-// s3.upload(params, async function (err, data) {
-//     //handle error
-//     if (err) {
-//         console.log("Error", err);
-//         return false;
-//     }
-
-//     //success
-//     if (data) {
-//         console.log("Uploaded in:", data.Location);
-//         let response = await Axios.post("/api/images/add", {
-//             filename: filename,
-//             label: label,
-//             image_size: imageSize,
-//             productID: productID,
-//         });
-//         if (response.status(201)) {
-//             return true;
-//         }
-//         return false;
-//     }
-// });
-
-// var params = {
-//     Key: "folder/1610482949524_sample.txt",
-//     Bucket: BUCKET_NAME,
-// };
-// s3.getObject(params, function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     fs.writeFileSync("./sample1.txt", data.Body);
-//     console.log("file downloaded successfully");
-// });
+export const sendMessage = (topic, to, type, message, time) => {
+    Axios.post('/api/messages/send', {topic, to, type, message, time})
+}
