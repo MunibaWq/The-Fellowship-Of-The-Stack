22d338fd228a3e977e081c98a019362e9aa6a010
danibarker <caseybarker86@gmail.com>
2021-02-20 06:45:10 -0700

    set up Messages page to use MessageList and MessageThread components

--- frontend/versa/src/pages/Shared/Messages.js
+++ frontend/versa/src/pages/Shared/Messages.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import MessageList from "../../components/Dashboard/Messages/MessageList";
 import { getMessages } from "../../axios/gets";
+import MessageThread from "../../components/Dashboard/Messages/MessageThread";
 
 const Messages = () => {
+    const [messages, setMessages] = useState();
+    const [thread, setThread] = useState()
     useEffect(() => {
         const getMessageList = async () => {
             const response = await getMessages();
-            console.log(response);
+        
+            response.sort((one, two) => {
+                return new Date(one.time) - new Date(two.time)
+            })
+            
+            
+            setMessages(response);
         };
         getMessageList();
     }, []);
@@ -15,18 +24,28 @@ const Messages = () => {
     return (
         <Container>
             <h1>Messages</h1>
-            <MessageList />
+            {messages && (
+                <MessageSection>
+                    <MessageList setThread={setThread} messages={messages} />
+                    <MessageThread thread={thread}/>
+                </MessageSection>
+            )}
         </Container>
     );
 };
 
 export default Messages;
-
+const MessageSection = styled.div`
+    display: grid;
+    grid-template-columns: 40% 60%;
+    height: 80vh;
+`;
 const Container = styled.div`
     width: 100vw;
     padding: 5em 2em;
     display: grid;
     grid-template-rows: 80px auto;
+
     /* justify-content:center; */
     min-height: 100vh;
 `;
